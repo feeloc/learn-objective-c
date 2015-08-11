68ec63a82d070071a02803ac70877538162c8cc5
feeloc <hjm1989@163.com>
2015-08-11 14:56:26 +0800

    commit foundation demos

---
+++ foundation/foundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  foundation
+//
+//  Created by hujianmeng on 15/8/11.
+//  Copyright (c) 2015年 hujianmeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
