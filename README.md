f992cdb1025eac814602a3c9cf2597239fad2f08
feeloc <hjm1989@163.com>
2015-08-11 16:19:39 +0800

    NSString

--- README.md
+++ README.md
@@ -32,4 +32,25 @@ Esc ：显示代码提示菜单
 Xcode 代码格式化/自动排版 右击 ->Structure ->Re-Indent
 ```
 
+## 二、基本语法
+
+1. NSString
+```objective-c
+
+// C、OC字符串相互转换
+char *s = "hello c";
+NSLog(@"C语言字符串：%s", s);
+
+NSString *str = @"hello objective c";
+NSLog(@"objective c 字符串：%@", str);
+
+// C转OC
+NSString *str1 = [NSString stringWithUTF8String:s];
+NSLog(@"C转OC：%@", str1);
+
+//OC转C
+NSLog(@"OC转C：%s", [str UTF8String]);
+
+```
+
     
--- foundation/foundation/main.m
+++ foundation/foundation/main.m
@@ -10,11 +10,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-        NSString *a = @"aaa";
-        NSLog(@"%@",a);
-        NSLog(@"%@", @"AAAA");
+        // C、OC字符串相互转换
+        char *s = "hello c";
+        NSLog(@"C语言字符串：%s", s);
+        
+        NSString *str = @"hello objective c";
+        NSLog(@"objective c 字符串：%@", str);
+        
+        // C转OC
+        NSString *str1 = [NSString stringWithUTF8String:s];
+        NSLog(@"C转OC：%@", str1);
+        
+        //OC转C
+        NSLog(@"OC转C：%s", [str UTF8String]);
     }
     return 0;
 }
